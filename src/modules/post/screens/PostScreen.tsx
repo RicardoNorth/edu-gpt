301ebb6655cedf109d5a381ed56dd0e903cd4d17
RicardoNorth <litie326@163.com>
2025-04-19 12:15:04 +0800

    chore: 初始化项目结构（React Native + Navigation + Modules）

--- src/modules/post/screens/PostScreen.tsx
+++ src/modules/post/screens/PostScreen.tsx
@@ -1,10 +1,10 @@
-// src/screens/PostScreen.tsx
+// src/modules/post/screens/PostScreen.tsx
 import { View, Text, StyleSheet } from 'react-native'
 
 export default function PostScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>这是帖子页面</Text>
+      <Text style={styles.text}>这是发帖页面</Text>
     </View>
   )
 }
--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,7 +1,6 @@
 // src/navigation/RootNavigator.tsx
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeScreen from '../screens/HomeScreen'
-import PostScreen from '../screens/PostScreen'
+import PostScreen from '../modules/post/screens/PostScreen' // 👈 模块路径
 
 export type RootStackParamList = {
   Home: undefined
@@ -13,7 +12,6 @@ const Stack = createNativeStackNavigator<RootStackParamList>()
 export default function RootNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Posts" component={PostScreen} />
     </Stack.Navigator>
   )
